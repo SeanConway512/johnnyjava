183a95c37e394f0796f4ed461217be254bd0968d
SeanConway512 <Sean.Conway@students.ittralee.ie>
2016-09-22 10:08:06 +0100

    Add files via upload

---
+++ happy.java
@@ -0,0 +1,9 @@
+//*happy.java*
+/*This program displays my name,the date and a happy message*/
+{
+public class happy
+public static void main(String args[])
+{
+  system.out.println("the date is the 22nd,my name is sean and im batman")
+}
+}
